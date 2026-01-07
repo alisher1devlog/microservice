79e8c60699cfd35932c4e8cd8db232d207c85a45
Alisher <alisheryondoshaliyev77@gmail.com>
2026-01-07 11:32:48 +0500

    updates

--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -1,8 +1,37 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Teachers servisiga yo'naltirish
+  app.use(
+    '/teachers',
+    createProxyMiddleware({
+      target: 'http://localhost:3001',
+      changeOrigin: true,
+    }),
+  );
+
+  // Students servisiga yo'naltirish
+  app.use(
+    '/students',
+    createProxyMiddleware({
+      target: 'http://localhost:3002',
+      changeOrigin: true,
+    }),
+  );
+
+  // Products servisiga yo'naltirish
+  app.use(
+    '/products',
+    createProxyMiddleware({
+      target: 'http://localhost:3003',
+      changeOrigin: true,
+    }),
+  );
+
+  await app.listen(8000);
 }
 bootstrap();
